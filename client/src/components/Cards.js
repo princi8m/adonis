175eb50ca64d43f375b5c9999fbf1f2ce2ef7fc1
oezge sebisteri <sebisteri@outlook.com>
2021-03-08 10:44:33 +0100

    adds visual appearance

---
+++ client/src/components/Cards.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Start Training!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/img-9.jpg'
+              text='Find the right exercises for your training plan with exercise instructions and pictures.'
+              label='Exercise Catalogue'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-2.jpg'
+              text='
+Create and edit your personal fitness plan and share your progress with your coach.'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          {/* <ul className='cards__items'>
+            <CardItem
+              src='images/img-3.jpg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src='images/img-4.jpg'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src='images/img-8.jpg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
